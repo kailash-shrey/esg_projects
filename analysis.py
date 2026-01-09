a99fbc6381bbb41f88b5b7b731c5db702ac4d23f
Shrey <kailash.shrey@gmail.com>
2026-01-09 18:42:46 +0530

    Learning for & if-else loop

--- analysis.py
+++ analysis.py
@@ -1,6 +1,16 @@
 company_name = "GreenCorp"
-emissions_2024 = 500.0
-emissions_2025 = 450.0
+emissions = [500.0, 480.0, 420.0, 390.0, 350.0]
 
-reduction = emissions_2024 - emissions_2025
-print(f"{company_name} reduced emissions by {reduction} tons.")
+total_emissions = sum(emissions)
+number_of_years = len(emissions)
+average_emissions = total_emissions / number_of_years
+
+print(f"Average annual emissions: {average_emissions} tons.")
+
+print("---Annual Check---")
+for year_emission in emissions:
+	if year_emission < 400:
+		print(f"{year_emission} is a 'Low Emission' year.")
+	else:
+		print(f"{year_emission} is a 'High Emission' year.")
+
